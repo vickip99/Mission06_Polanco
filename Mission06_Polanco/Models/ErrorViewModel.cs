3479865f2984aa4470de98fd4572fdf4fb541923
vicki polanco <vickyp812@gmail.com>
2024-02-13 23:05:54 -0700

    Add project files.

---
+++ Mission06_Polanco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Polanco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
